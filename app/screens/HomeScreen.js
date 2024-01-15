e2f08926080a2b471d23a8ba440fde39bbcececa
Julio Garcia <juliogarciag99@gmail.com>
2024-01-14 21:10:15 -0500

    Añadí boton

--- app/screens/HomeScreen.js
+++ app/screens/HomeScreen.js
@@ -13,6 +13,12 @@ export const Home = ({ navigation }) => {
                         navigation.navigate('ContactsNav');
                     }}
                 />
+                <Button
+                    title='Galeria'
+                    onPress={() => {
+                        navigation.navigate('ContactsNav');
+                    }}
+                />
             </View>
 
 
